66eccc1304b9a009284bcd7ce757aeef9a860449
Ivan Manan <ivanmanan@gmail.com>
2023-06-14 14:37:59 -0700

    Added basicCalculator

--- basicCalculator.cpp
+++ basicCalculator.cpp
@@ -54,8 +54,55 @@ int calculator(string s) {
 	return sum;
 }
 
+
+int basicCalculatorII(string s) {
+	stack<int> st;
+	int curr = 0;
+	char op = '+';
+	s += '+'; // Finale is necessary
+
+	for(int i = 0; i < s.length(); i++) {
+		if(isdigit(s[i])) {
+			curr = curr * 10 + int(s[i] - '0');
+		}
+		else if(s[i]=='+' || s[i]=='-' || s[i]=='*' || s[i]=='/') {
+			if(op == '+') {
+				st.push(curr);
+			}
+			else if(op == '-') {
+				st.push(-curr);
+			}
+			else if(op == '*') {
+				int num = st.top() * curr;
+				st.pop();
+				st.push(num);
+			}
+			else if(op == '/') {
+				int num = st.top() / curr;
+				st.pop();
+				st.push(num);
+			}
+			op = s[i];
+			curr = 0;
+		}
+
+	}
+
+	int res = 0;
+	while(!st.empty()) {
+		res += st.top();
+		st.pop();
+	}
+	return res;
+}
+
 int main() {
 	// cout << calculator("10+4-3") << endl;
 	// cout << calculator("205+310-105") << endl;
-	cout << calculator("43943+439149-594+9104+4043-4399-31000+440");
+
+// 	int ans = basicCalculatorII("3+2*2");
+// 	cout << "INPUT: 3+2*2; OUTPUT: " << ans << "; EXPECTED: 7";
+
+	int ans = basicCalculatorII("1337");
+	cout << "INPUT: 1337; OUTPUT: " << ans << "; EXPECTED: 1337";
 }
