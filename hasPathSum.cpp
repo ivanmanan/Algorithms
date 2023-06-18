1281f932f9d20ed083aec7535802ce18cb386fc7
Ivan Manan <ivanmanan@gmail.com>
2023-06-18 13:16:53 -0700

    Updated

---
+++ hasPathSum.cpp
@@ -0,0 +1,11 @@
+bool hasPathSum(TreeNode* root, int targetSum) {
+    if(root == NULL) {
+        return false;
+    }
+
+    if(root->val == targetSum && !root->left && !root->right) {
+        return true;
+    }
+    return hasPathSum(root->right, targetSum - root->val) || hasPathSum(root->left, targetSum - root->val);
+
+}
